bb1ba505c940f7b8cf531948556f706ab40671be
dbsalazarr <dbsalazarr@gmail.com>
2026-09-13 20:10:30 -0500

    Algoritmo calculo de componentes
    Se encarga de realizar todas las combinaciones posibles para los valores de capacitancias definidos

--- 1. ANALISIS EN FRECUENCIA/CALCULO COMPONENTES/components_calc.py
+++ 1. ANALISIS EN FRECUENCIA/CALCULO COMPONENTES/components_calc.py
@@ -1,17 +1,14 @@
 """
     ALGORITMO PARA CALCULAR LOS COMPONENTES RESISTIVOS EN FUNCIÓN A LAS CAPACITANCIAS Y LOS POLOS Y CEROS DE LA FUNCIÓN DE TRANSFERENCIA PARA UNA PLANTA DE 3ER ORDEN
 """
-
-# CONSTANTES
-C1 = 10*10**-6
-C3 = 47*10**-6
 """ 
 Definiendo los coeficientes de la función de transferencia obtenida
 Expresión de 2do orden
     A = s^2
     B = S
     C = 1er TI
-Expresión de 3er orden
+
+Expresión de 1er orden
     D = S
     E = 2do TI
 
@@ -30,21 +27,42 @@ G = 100
 El objetivo es determinar el valor de resistencias para la implementación
 R1=R2=R3=R; R4; R5
 Y el valor de capacitancia para C2
-
 """
-def calculo_resistencias(A, B, C, D, E, G):
+# CONSTANTES
+caps1 = [100, 120, 160, 220, 270, 330, 390, 470, 680, 1000, 2200, 4700, 10000]
+C1 = [cap*10**-9  for cap in caps1]
+
+caps3 = [100, 120, 160, 220, 270, 330, 390, 470, 680, 1000, 2200, 4700, 10000]
+C3 = [cap*10**-9 for cap in caps3]
+
+def calculo_resistencias(A, B, C, D, E, G, C1, C3) :
+
     R = 3/(C1*B)
-    C2 = B**2/((C1**3)*9*C)
+    C2 = B**2*C1/(9*C)
     R4 = 1/(G*C1*C2*C3*R**2)
     R5 = 1/(E*C3)
 
     return [R, C2, R4, R5]
 
+def valores_optimizados(C1, C3) :
+
+    resultados = []
+
+    for i in C1 :
+        for j in C3 :
+            resultados.append(calculo_resistencias(A, B, C, D, E, G, i, j))
+
+    return resultados            
+    
 def main() :
-    resultados = calculo_resistencias(A, B, C, D, E, G)
-    print("Los resultados para: R, C2, R4 Y R5 son:")
-    for resultado in resultados :
-        print(resultado)
+    lista_resultados = valores_optimizados(C1, C3)
+    print("RESULTADOS")
+    print("R, C2, R4, R5")
+    i = 0
+    for ca1 in C1 :
+        for ca3 in C3 :
+            print(f'C1 = {ca1}, C3 = {ca3} : {lista_resultados[i]}')
+            i = i + 1   
 
 main()
 
